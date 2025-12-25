3ed9509c5ca7c99e9c80cfa9a936726d29baf70d
dagabriel31 <dagabriel31@gmail.com>
2025-12-24 21:17:04 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // No Netlify, usamos apenas a barra para indicar a raiz
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
